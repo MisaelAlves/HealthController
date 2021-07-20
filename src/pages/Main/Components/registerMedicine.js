58d1178d58176007ce3b2b7051bd80df30ea03d1
juancalheiros0001@gmail.com <juancalheiros0001@gmail.com>
2021-07-19 22:40:31 -0300

    feat: add registerMedicine component

--- src/pages/Main/Components/registerMedicine.js
+++ src/pages/Main/Components/registerMedicine.js
@@ -1,12 +1,42 @@
 import React from 'react';
 import {Text,View} from 'react-native';
 
-const RegisterMedicine = () => {
-    return (
-        <View>
-            <Text> OK, Funcionou </Text>
-        </View>
+import SalveButton from './salveButton';
+import CancelButton from './cancelButton';
+
+const RegisterMedicine = (props) => {
+
+    const { handleClickRegisterMedicine } = props
+
+
+    return (   
+        <>
+            <View>
+                 <Text> Componente: Periodo de despertar o alarme </Text>
+            </View>
+            
+            <View>
+                 <Text> Componente:  Title </Text>
+            </View>
+
+            <View>
+                 <Text> Componente:  Descrição </Text>
+            </View>
+
+            <View>
+                 <Text> Componente:  Som </Text>
+            </View>
+
+            <View>
+                 <Text> Componente:  Vibração </Text>
+            </View>
+           
+            <SalveButton handleClickRegisterMedicine={handleClickRegisterMedicine} />
+        
+            <CancelButton handleClickRegisterMedicine= {handleClickRegisterMedicine} />
+        </>
     )
 }
+
 
 export default RegisterMedicine
